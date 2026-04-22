37b2efaffde51fa0015d6b78aeea7214ec040773
ChrisALPuig <sadpikachu350@gmail.com>
2026-04-22 16:30:27 +0200

    Error main.ts Vercel

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
-import { router } from './router/index.js' // asegúrate que la ruta es correcta
+import { router } from './router/index.ts' // asegúrate que la ruta es correcta
 
 const app = createApp(App)
 
